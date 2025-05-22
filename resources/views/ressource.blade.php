b9132e5b00343bd7319fd68b57ef2f12ec0a37f8
ImraneBendassi <imrane.bendassi@laplateforme.io>
2025-05-22 14:35:55 +0200

    Ajout de la barre de recherche

--- resources/views/ressource.blade.php
+++ resources/views/ressource.blade.php
@@ -66,6 +66,22 @@
  <br class="hidden md:block" />
    et atteindre vos objectifs financiers.
   </p>
+
+<div class="flex justify-center mt-10">
+  <div class="relative w-full max-w-md">
+    <input
+      type="text"
+      placeholder="Rechercher..."
+      class="w-full pl-10 pr-4 py-2 border rounded-2xl shadow focus:outline-none focus:ring-2 focus:ring-blue-500"
+    />
+    <div class="absolute left-3 top-2.5 text-gray-400">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
+        viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 104.5 4.5a7.5 7.5 0 0012.15 12.15z" />
+      </svg>
+    </div>
+  </div>
 <script src="https://cdn.tailwindcss.com"></script>
 </body>
 </html>
