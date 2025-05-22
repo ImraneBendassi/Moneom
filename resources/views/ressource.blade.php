e6e7cf5ccc35ef34813ab0f5dd87b15608aed5da
ImraneBendassi <imrane.bendassi@laplateforme.io>
2025-05-22 11:05:05 +0200

    Changement de la couleur de fond de la page

--- resources/views/ressource.blade.php
+++ resources/views/ressource.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ressource</title>
 </head>
-<body>
+<body class="bg-sky-950">
 <header class="bg-gray-800 text-white py-4">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="flex items-center justify-between">
